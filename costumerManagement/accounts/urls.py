54e4146adacc8e4ef30acb4289a45e832268c2f2
amit-c-ai <amitchoudhary9425@gmail.com>
2021-10-08 13:56:34 +0530

    adding images

--- costumerManagement/accounts/urls.py
+++ costumerManagement/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
     path('costumers/', views.costumers),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
