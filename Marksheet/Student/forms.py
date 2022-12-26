8b457cccdd1d3f4451b67592a9745942fef1f898
amit-c-ai <amitchoudhary9425@gmail.com>
2022-12-26 22:08:41 +0530

    Done

--- Marksheet/Student/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Enter_Detail
-
-
-class Enter_DetailForm(ModelForm):
-	class Meta:
-		model = Enter_Detail
-		fields = ['reg']
--- Marksheet/Student/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.result),
-    path('result/', views.result, name="result"),
-    path('resultpage/<str:pk>/', views.resultpage, name="resultpage"),
-]
